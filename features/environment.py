a671e86cae0e61ff364007bd6d1a9d9c597a24fb
mizdeedz <jessicactinsley@gmail.com>
2021-11-02 17:46:54 -0700

    add lesson 12 files

--- features/environment.py
+++ features/environment.py
@@ -1,33 +1,77 @@
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 
-def browser_init(context):
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.events import EventFiringWebDriver
+
+from support.logger import logger, MyListener
+
+# Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+bs_user = 'jessicatinsley_oaecDv'
+bs_pw = 'ehYm5xf9jCRmUM7e6UpQ'
+
+# Allure command to wrap:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
+
+# Allure command to show results:
+# allure serve test_results/
+
+def browser_init(context, test_name):
     """
     :param context: Behave context
+    :param test_name: scenario.name
     """
     context.driver = webdriver.Chrome(r'C:\Users\jessi\repo\qa_auto\python-selenium-automation\chromedriver_win32\chromedriver.exe')
-    # context.browser = webdriver.Safari()
-    # context.browser = webdriver.Firefox()
+    # context.driver = webdriver.Safari()
+    # context.driver = webdriver.Firefox(executable_path='/Users/svetlanalevinsohn/JobEasy/7-python-selenium-automation/geckodriver')
+
+    ## HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(chrome_options=options)
+
+    ### EventFiringWebDriver - log file ###
+    ### for drivers ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(), MyListener())
+    # for headless mode ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
+
+    ### for browerstack ###
+    # desired_cap = {
+    #     'browser': 'Chrome',
+    #     'browser_version': '94',
+    #     'os': 'Windows',
+    #     'os_version': '10',
+    #     'name': test_name
+    # }
+    # url = f'http://{bs_user}:{bs_pw}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     context.driver.maximize_window()
-    context.driver.implicitly_wait(4)
+    context.driver.implicitly_wait(5)
     context.driver.wait = WebDriverWait(context.driver, 10)
+
     context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    # logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
     print('\nStarted step: ', step)
+    # logger.info(f'Started step: {step}')
 
 
 def after_step(context, step):
     if step.status == 'failed':
+        # logger.error(f'Step failed: {step}')
         print('\nStep failed: ', step)
+        # Mark test case as FAILED on BrowserStack:
+        # context.driver.execute_script(
+        #     'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "Step failed"}}')
 
 
 def after_scenario(context, feature):
